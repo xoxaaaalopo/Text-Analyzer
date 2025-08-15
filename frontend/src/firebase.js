0bd0959e0b43e791733c4b7aee1d7fce17aeb4c5
xoxaaaalopo <aoto15530@gmail.com>
2025-08-15 12:06:02 -0700

    Hide API keys

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -8,13 +8,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCwBtdfXii8N6TSnT4nJTJn2foP9mT9wOg",
-  authDomain: "find-advanced-words.firebaseapp.com",
-  projectId: "find-advanced-words",
-  storageBucket: "find-advanced-words.firebasestorage.app",
-  messagingSenderId: "730070301084",
-  appId: "1:730070301084:web:3eee67141ff877b9f7b059",
-  measurementId: "G-5CLJTZQSMY"
+  
 };
 
 // Initialize Firebase
